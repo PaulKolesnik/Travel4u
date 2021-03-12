63c477e6e4382e32180932b1fb0fc26c33fde21a
edenmoz22 <mozesita22@gmail.com>
2021-03-13 01:38:24 +0200

    changeUI

--- Client/Travel4u/src/app/components/register/register.component.ts
+++ Client/Travel4u/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  public formRegister: FormGroup = new FormGroup({ FullName: new FormControl('', Validators.required) });
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  public Submit() { console.log(this.formRegister); }
 
 }
