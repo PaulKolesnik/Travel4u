184d300358492f6ecee43fcd647c877dd9cd80b7
Paul Kolesnik <paulkoles18@gmail.com>
2021-03-13 02:58:53 +0200

    page-not-found

--- Client/Travel4u/src/app/components/footer/footer.component.ts
+++ Client/Travel4u/src/app/components/footer/footer.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
+  date: Date;
+  year: number;
   constructor() { }
-
+  
   ngOnInit(): void {
+    this.date = new Date();
+    this.year = this.date.getFullYear();
   }
 
 }
