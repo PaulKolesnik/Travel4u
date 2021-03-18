f262d44d6c3e87d024f336850213afebe73917d0
Paul Kolesnik <paulkoles18@gmail.com>
2021-03-18 16:35:05 +0200

    fix eslint json

--- Server/src/server.js
+++ Server/src/server.js
@@ -1,27 +1,28 @@
-const express = require('express');
+/* eslint-disable no-undef */
+const express = require("express");
 const app = express(),
-      bodyParser = require("body-parser");
-      port = 3080;
+  bodyParser = require("body-parser");
+  port = 3080;
 
 const users = [];
 
 app.use(bodyParser.json());
-app.use(express.static(process.cwd()+"/Server/public"));
+app.use(express.static(process.cwd() + "/Server/public"));
 
-app.get('/api/users', (req, res) => {
+app.get("/api/users", (req, res) => {
   res.json(users);
 });
 
-app.post('/api/user', (req, res) => {
+app.post("/api/user", (req, res) => {
   const user = req.body.user;
   users.push(user);
   res.json("user addedd");
 });
 
-app.get('/', (req,res) => {
-      res.sendFile(process.cwd()+ "/Server/public/index.html")
+app.get("/", (req, res) => {
+  res.sendFile(process.cwd() + "/Server/public/index.html");
 });
 
 app.listen(port, () => {
-    console.log(`Server listening on the localhost:${port}`);
+  console.log(`Server listening on the localhost:${port}`);
 });
