fb4453b7bf0f770a666376bdc1cbdca61d3f07fe
Paul Kolesnik <paulkoles18@gmail.com>
2021-05-04 15:21:21 +0300

    rg

--- Server/src/server.js
+++ Server/src/server.js
@@ -35,8 +35,8 @@ app.use('/orders', require('../routes/order.js'));
 
 app.get("/", (req, res) => {
   //res.sendFile(__dirname + "/../public/index.html");
-  res.sendFile(path, {root: '/public/index.html'});
-  //res.sendFile('index.html', {root: /public/});
+  //res.sendFile(path, {root: '/public/index.html'});
+  res.sendFile('index.html', {root: __dirname});
 });
 
 app.listen(port, () => {
